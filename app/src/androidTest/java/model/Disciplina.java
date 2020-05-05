de614adb54365b32c84150568820648f3a39680c
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-05-05 14:51:54 -0300

    Criação da classe Disciplina

---
+++ app/src/androidTest/java/model/Disciplina.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Disciplina {
+
+    private String nome;
+    private String codigo;
+    private String turma;
+
+    public Disciplina(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+}
--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText nome;
+    private TextInputEditText codigo;
+    private TextInputEditText turma;
+    private Button limpar;
+    private Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.editTextNome);
+        codigo = findViewById(R.id.editTextCodigo);
+        turma = findViewById(R.id.editTextTurma);
+        limpar = findViewById(R.id.buttonLimpar);
+        cadastrar = findViewById(R.id.buttonCadastrar);
     }
 }
